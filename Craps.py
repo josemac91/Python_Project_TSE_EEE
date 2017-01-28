8b192a73183b1d83030c1b457ea0810edf4b959c
Jose Alvarez <josemanu.16ac@gmail.com>
2017-01-28 21:25:21 +0100

    Craps, documented code, updated w/ Ted

--- Craps.py
+++ Craps.py
@@ -1,120 +1,120 @@
 import random
 from random import randint
-# This is used to fixed the random generator so we can test the output
+#This is used to fix the random generator so we can test the output
 random.seed(3456)
 
-"""
-Keep in mind the following:
-- As in the Roulette, we need to ensure that players who bet below the table minimum lose
-- Players still have to decide on an amount to bet and on which outcome from 2 to 12 they bet money on
-- Similarly, we only care about the sum of the two dices; no colors are associated to the random outcome
-- The trick is that we need to account for the fact that not all outcomes have the same likelihood
-- We have to rescale the payoffs so that we don't have everybody betting on 7
-- We also have to ensure that the house wins, on average, 10% of the total amounts bet
-"""
+#This determines the payoffs and will be used to determine the winnings for the players
+#Scaling the payoffs based on probabilites so that the expected outcomes are the same across possible guesses
+def payouts(roll):
+    #The odds of rolling 2 or 12 is 1/36. the payoff is then $36 for every dollar bet IF 100% goes back to players
+    if roll == 2 or roll == 12:
+        pomult = 36
+    #For 3 and 11, the odds are 2/36 or 1/18. Thus the payoff is $18 for every dollar
+    elif roll == 3 or roll == 11:
+        pomult = 18
+    #This continues the pattern, but odds are now 3/36
+    elif roll == 4 or roll == 10:
+        pomult = 12
+    #Odds are 4/36 now
+    elif roll == 5 or roll == 9:
+        pomult = 9
+    #Odds are 5/36
+    elif roll == 6 or roll == 8:
+        pomult = 7.2
+    #This is for the roll equaling 7 which has probability of 6/36
+    else:
+        pomult = 6
+    #Casino makes a 10% profit. So payoffs must be multiplied by 0.9 for the players getting 90% back.
+    Casinowins = 0.9*pomult
+    #We will later multiply this by the wager of the winning players
+    return(Casinowins)
 
-def AboveMinimum(betted_amounts):
-    min_amount = 10
-    boolist = []
-    for i in range(0, len(betted_amounts)):
-        if betted_amounts[i] < min_amount:
-            boolist.append(False)
+#A function for the boolian check of meeting the minimum bet (set at 10 here)
+def AboveMinimum(dollars):
+    boollist = []
+    #Arbitrarily chosen (no guideline given)
+    minimumbets = 10
+    #Loop for each player
+    for i in range(0, len(dollars)):
+        #If the bet is above the min, it returns TRUE
+        if (dollars[i] > minimumbets):
+            boollist.append(True)
+        #If not, it is an invalid bet and receives the FALSE response
         else:
-            boolist.append(True)
-    return(boolist)
+            boollist.append(False)
+    return(boollist)
 
-# AboveMinimum is the same as the one in Roulette
-# Note that minimum amount is arbitrarily chosen (we had no example to guide our decision)
-
+#Here we find out what the sum of dice is
 def Dices():
+    #To get the odds correct, we use two die instead of one.
     dice1 = randint(1, 6)
     dice2 = randint(1, 6)
-    sum_dices = dice1 + dice2
-    return(sum_dices)
+    #The sum of these two numbers is the correct number
+    rollbounce = dice1 + dice2
+    return(rollbounce)
 
-# Function Dices defines our outcome, which is the sum of two random variable
-# Clearly, not all outcomes has the same probability
-# For example, the probability of getting 12 is 1/36
-# Out of 36 possible combinations, there's only one that gives you 12, which is 6 + 6
-# This is only the case when the dice rolls are independent events
-
-def TheHouseAlwaysWins(dicedice):
-    if dicedice == 2 or dicedice == 12:
-        payoff = 36*0.9
-    elif dicedice == 3 or dicedice == 11:
-        payoff = 18*0.9
-    elif dicedice == 4 or dicedice == 10:
-        payoff = 12*0.9
-    elif dicedice == 5 or dicedice == 9:
-        payoff = 9*0.9
-    elif dicedice == 6 or dicedice == 8:
-        payoff = 7.2*0.9
+#The main function
+def RollTheDices(guesses, wagers):
+    #Prints the statement below
+    print("Throwing the dice...")
+    #Determines the sum of the dice
+    theroll = Dices()
+    #Stating the correct number
+    print("We have " + str(theroll))
+    #How many winners there are
+    numwinners = guesses.count(theroll)
+    #If there are no winners
+    if (numwinners == 0):
+        playerwinnings = []
+        print("There are no winners")
+        #Casino wins the wagers
+        casinowinnings = sum(wagers)
+        #Players earn nothing
+        for i in range(0, len(wagers)):
+            playerwinnings.append(0)
+        #Final output
+        winningsoverall = [casinowinnings, playerwinnings]
+        print(winningsoverall)
+        return(winningsoverall)
+    #If there is at least one winner
     else:
-        payoff = 6*0.9
-    return(payoff)
-
-# The probability that the sum of the dice equal 2 or 12 is 1/36
-# And the probability for getting 3 or 11 is 2/36
-    # For 3: Prob(dice1 = 1) x Prob(dice2 = 2) + Prob(dice1 = 2) x Prob(dice2 = 1) = 1/36 + 1/36 = 2/36
-# And the probability of getting: 4 or 10 is 3/36; 5 or 9 is 4/36; 6 or 8 is 5/36; and 7 9s 6/36
-# To rescale the game each outcome is multiplied by the reciprocal of its probability
-# Now players who bet on 2 (one of the less probably outcomes) receives 36 times his bet amount
-# While players who bet on 7 (the most probable outcome) receives 6 times his bet amount
-# We insure Casino's on average 10% profit by making players only receive 90% of their total earnings
-
-# RollTheDices gives the structure of the Craps
-# It's a function of the players: bet amounts (ba), the guess numbers (gn), the eligibility (boolist)
-# As with the Roulette, we first insure those below the minimum always lose
-
-def RollTheDices(ba, gn, boolist):
-    print('Rolling the dices...')
-    sd = Dices()
-    ngn = []
-    # ngn stands for new guess numbers
-    for i in range(0, len(ba)):
-        if boolist[i] == True:
-            ngn.append(gn[i])
-        else:
-            ngn.append(13)
-        # those not eligible to win are assigned the ngn 13 and thus always lose
-    print('And we have ' + str(sd))
-    numwin = ngn.count(sd)
-    # here we know the sum of the dices and the number of winners
-    # either nobody wins or at least one player wins
-    if numwin == 0:
-        print('No winners this round')
-        casino_wins = sum(ba)
-        # the casino takes it all
-        players_lose = []
-        for i in range(0, len(ba)):
-            players_lose.append(0)
-        # all players lose and are assigned a 0 as their earnings
-        outcome1 = [casino_wins, players_lose]
-        print(outcome1)
-    else:
-        print('We have ' + str(numwin) + ' winner(s)')
-        casino_wins = []
-        player_wilo = []
-        for i in range(0, len(ba)):
-            if ngn[i] == sd:
-                casino_wins.append(0)
-                player_wilo.append(ba[i]*TheHouseAlwaysWins(sd))
-            # when a player wins, the casino loses that player's bet amount
-            # winning player gets his ba times the rescaling of payoffs proposed previously
-            # sd gives the random sum of the two dices
-            # and allocates the correct payoff to that number as defined in TheHouseAlwaysWins
+        #How many won
+        print(str(numwinners) + " player(s) won!")
+        playerwinnings = []
+        casinopwinnings = []
+        #For each bet
+        for i in range(0, len(wagers)):
+            #If they were correct, they get the payout based on the roll multiplied by the wager
+            #The casino gets nothing there
+            if (guesses[i]==theroll):
+                playerwinnings.append(payouts(theroll)*wagers[i])
+                casinopwinnings.append(0)
+            #For those that lost
             else:
-                casino_wins.append(ba[i])
-                player_wilo.append(0)
-            # losing players get 0
-            # the casino keeps their amounts bet
-        t_casino_wins = sum(casino_wins)
-        outcome2 = [t_casino_wins, player_wilo]
-        print(outcome2)
+                #The players get nothing while the casino gets their wager
+                playerwinnings.append(0)
+                casinopwinnings.append(wagers[i])
+        #Casino is transformed from per player winnings to a total
+        casinowinnings = sum(casinopwinnings)
+        #Final output
+        winningsoverall = [casinowinnings, playerwinnings]
+        print(winningsoverall)
+        return(winningsoverall)
 
-# The simulation is based on the above functions
-# Note that guess numbers (guess) and amount bet (money) of the players have to be given
-
-def SimulateGame(guess, money):
-    min_min = AboveMinimum(money)
-    RollTheDices(money, guess, min_min)
+#Final command
+def SimulateGame(guesses, wagers):
+    nguess = []
+    #Checks if the bets are ok
+    Checking = AboveMinimum(wagers)
+    #Makes new guesses based on validity of bets.
+    #If the bet is above the min, then the nguess (new guess) is the same as the old
+    #If the bet is below the min, the guess is changed to 0 which is never a correct guess
+    #This way, the invalid bets are never winners
+    for i in range(0, len(Checking)):
+        if (Checking[i]==True):
+            nguess.append(guesses[i])
+        else:
+            nguess.append(0)
+    #Run the roll the dices based on the new guesses (nguess)
+    out = RollTheDices(nguess, wagers)
+    return(out)
