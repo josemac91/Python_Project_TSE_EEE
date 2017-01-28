08c22887732db79a1a5a6b34550a57000e7a8ba2
Jose Alvarez <josemanu.16ac@gmail.com>
2017-01-28 21:27:36 +0100

    Simulation for Roulette and Craps, updated w/ Ted

--- Simulation.py
+++ Simulation.py
@@ -1,40 +1,18 @@
 import Roulette
-allbets = [10, 24, 36, 0, 11, 24]
-allamounts = [10, 85, 120, 65, 150, 122]
-Roulette.SimulateGame(allbets, allamounts)
-Roulette.SimulateGame(allbets, allamounts)
-
 import Craps
-crapsguesses = [7, 9, 2, 10, 3]
-crapsbets = [50, 70, 30, 40, 50]
-Craps.SimulateGame(crapsguesses, crapsbets)
-Craps.SimulateGame(crapsguesses, crapsbets)
 
-"""
-Using random.seed(3456) and the corresponding minimum amounts, you should get for the above inputs...
+#The guesses
+allbets = [10, 24, 36, 0, 11, 24]
+#The wagers
+allamounts = [10, 85, 120, 65, 150, 122]
+#Running the roulette game twice
+Roulette.SimulateGame(allbets, allamounts)
+Roulette.SimulateGame(allbets, allamounts)
 
-For Roulette:
-
-Spinning the wheel...
-Ball lands on 24
-We have 1 winner(s)
-[430, [0, 0, 0, 0, 0, 3660]]
-
-Spinning the wheel...
-Ball lands on 30
-No winners this round
-[552, [0, 0, 0, 0, 0, 0]]
-
-For Craps:
-
-Rolling the dices...
-And we have 10
-We have 1 winner(s)
-[200, [0, 0, 0, 432.0, 0]]
-
-Rolling the dices...
-And we have 7
-We have 1 winner(s)
-[190, [270.0, 0, 0, 0, 0]]
-"""
-
+#Craps guesses
+crapguesses = [7, 9, 2, 11, 3]
+#The wagers for craps
+crapbets = [50, 70, 30, 40, 50]
+#Simulating a game of craps
+Craps.SimulateGame(crapguesses, crapbets)
+Craps.SimulateGame(crapguesses, crapbets)
